7c2de7b3b5e525260c8cae103227b2fee7ad1b07
ivan <vansmuhammad@gmail.com>
2019-05-21 22:44:05 +0700

    Rendering list with detail song

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Learn React</header>
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
